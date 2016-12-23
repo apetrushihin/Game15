f83e7603f06c73be5b59ea6dd9be5d4db220eba2
Aleksandr Petrušihin <aleksandr.petrushihin@gmail.com>
2016-12-23 18:05:09 +0200

    slightly edited GUI, added vars

--- src/Game15.java
+++ src/Game15.java
@@ -1,45 +1,66 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.layout.GridPane;
+
+import java.util.Random;
 
 public class Game15 extends Application {
 
-    BorderPane layout;
+    private GridPane layout = new GridPane();
+    Stage gameWindow;
+    private static Random generator = new Random();
+    private int[][] numbers = new int[4][4];
+    private MenuBar menu;
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("15 puzzle");
+
+        gameWindow = primaryStage;
+
+        gameWindow.setTitle("15 puzzle");
+        gameWindow.setResizable(false);
+        createMenu();
+        layout.setPadding(new Insets(0,0,0,0));
+        VBox vBox = new VBox();
+        Scene scene = new Scene(vBox, 390, 415);
+        vBox.getChildren().addAll(createMenu(), layout);
+
+        gameWindow.setScene(scene);
+        gameWindow.show();
 
 
+
+    }
+
+    private MenuBar createMenu(){
+        menu = new MenuBar();
         Menu gameMenu = new Menu("Game");
 
         MenuItem newGame = new MenuItem("New Game");
-        gameMenu.getItems().add(newGame);
 
 
         MenuItem exitGame = new MenuItem("Exit");
-        gameMenu.getItems().add(exitGame);
-        exitGame.setOnAction(e -> primaryStage.close());
+        exitGame.setOnAction(e -> gameWindow.close());
 
         MenuBar gameMenuBar = new MenuBar();
         gameMenuBar.getMenus().addAll(gameMenu);
 
-        layout = new BorderPane();
-        layout.setTop(gameMenuBar);
-
-        Scene scene = new Scene(layout, 500, 500);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        gameMenu.getItems().addAll(newGame, exitGame);
+        menu.getMenus().add(gameMenu);
+        return menu;
     }
 
+    public static void main(String[] args) {
+
+        launch(args);
+    }
 
 }
